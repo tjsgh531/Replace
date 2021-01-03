37b2748976104639bc2a45d9796369923d40368c
Guest <guest-8czns6@h26632.sgb.skir.kr>
2021-01-03 20:43:16 +0900

    first_daft

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-//  #submitButton 클릭 시의 처리 작업 설정
-document.querySelector('#submitButton').addEventListener('click', (event) => {
-    // 전화번호 가져오기
-    const phoneNumber = document.querySelector('#phoneNumberText').value;
-  
-    // 전화번호에 하이픈(-)이 삽입된 경우 ‘ ‘(공백)으로 변환
-    const trimmedPhoneNumber = phoneNumber.replace(/-/g, '');
-    // 01012345678
-    alert(`전화번호는 ${trimmedPhoneNumber}입니다.`);
-  
-    // 버튼의 기본 작동을 해제
-    event.preventDefault();
-  });
+const inputText = document.getElementById('inputSection');
+const Button= document.getElementById('submitBtn');
+
+Button.addEventListener('click',onClick);
+
+function onClick(event){
+    const textValue = inputText.value;
+    let result;
+    result = textValue.replace(/-/g,'');
+    window.alert(result);
+
+    //e.preventDefault();//요놈 왜 안될까?
+}
